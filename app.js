d95199547098e0b637ac0b7b347b8e564bfa336c
fantasyzernontra <fantasyzernontra@hotmail.com>
2020-12-24 00:41:00 +0700

    [ADD] Heroku Deployment

--- app.js
+++ app.js
@@ -8,4 +8,4 @@
     res.sendFile(path.join(__dirname, 'build', 'index.html'))
  })
 
- app.listen(9000, () => console.log('Server is listening on 9000'))
+ app.listen(process.env.PORT || 5000, () => console.log('Server is listening on 9000'))
