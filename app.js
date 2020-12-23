c7d733991aac0ee5a2c94f0cce93d6077b85b322
fantasyzernontra <fantasyzernontra@hotmail.com>
2020-12-24 00:44:03 +0700

    [ADD] Heroku Deployment

--- app.js
+++ app.js
@@ -2,10 +2,12 @@
  const path = require('path')
  const app = express()
 
+ const PORT = process.env.PORT || 5000
+
  app.use(express.static(path.join(__dirname, 'build')))
 
  app.get('/*', (req, res) => {
     res.sendFile(path.join(__dirname, 'build', 'index.html'))
  })
 
- app.listen(process.env.PORT || 5000, () => console.log('Server is listening on 9000'))
+ app.listen(PORT , () => console.log(`Server is listening on ${PORT}`))
